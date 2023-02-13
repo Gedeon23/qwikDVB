75cd6818e976550aa095b7e722366ed40850ee70
Gedeon Jahns <gedeon.jahns@web.de>
2023-02-13 22:47:51 +0100

    added  task for updating connections

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,15 +3,32 @@ import {
     useStore,
     createContext,
     useContextProvider,
+    useTask$,
 } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import { TripPlanner } from "~/components/tripPlanner/tripPlanner";
+import * as dvb from "dvbjs";
 
 export const AppState = createContext("app-state");
 
 export default component$(() => {
     const state = useStore({
         stops: [{}, {}],
+        connections: [],
+    });
+
+    useTask$(async ({ track }) => {
+        track(() => state.stops);
+        console.log("updating connections");
+        try {
+            state.connections = await dvb.route(
+                state.stops[0].id,
+                state.stops[1].id
+            );
+        } catch (error) {
+            console.log(error, state.stops);
+        }
+        console.log(state.connections);
     });
 
     useContextProvider(AppState, state);
